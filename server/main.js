4709a246f73363950d17c22afcf77a5ba71a5d3c
Maxwell Babey <maxwell@lys-ia.com>
2025-11-14 11:43:30 -0400

    Add README.md

--- server/main.js
+++ server/main.js
@@ -2,15 +2,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-/*
-Modify the code to do the following:
-- /route1 should check the request for the presence of an Authorization header
-  with the value “Bearer 1234” and should return an appropriate error code if
-  it does not exist.
-- /route1 and /route2 should attach a response header "Date" that is the time
-  UTC time the request was received.
-*/
-
 app.get("/route1", (req, res) => {
   res.send("This is Route 1");
 });
